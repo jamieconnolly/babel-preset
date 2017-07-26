f916646b15cbf7ba59e0740184c8adc234d334e6
Jamie Connolly <76968+jamieconnolly@users.noreply.github.com>
2017-07-26 14:02:22 +0100

    :white_check_mark: Add in some basic tests using Jest

---
+++ __tests__/basic.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var isPlainObj = require('is-plain-obj');
+
+it('test basic properties of config', function() {
+  var config = require('../index.js');
+
+  expect(isPlainObj(config)).toBe(true);
+});
