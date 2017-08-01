bc5afe740bc15db5c5db71f014b43e5c243c4305
Jamie Connolly <jamie@jamieconnolly.com>
2017-08-01 22:57:06 +0100

    :hammer: Refactor tests to be more jest-like

---
+++ __tests__/index.test.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const babel = require('babel-core');
+const isPlainObj = require('is-plain-obj');
+
+it('should export an object', function() {
+  const config = require('../index');
+
+  expect(isPlainObj(config)).toBe(true);
+  expect(config.presets).toBeTruthy();
+});
+
+it('should not contain invalid syntax', function() {
+  const result = babel.transform('let b = 1; a ** b;', {
+    babelrc: false,
+    presets: ['./index.js'],
+  });
+
+  expect(result).toBeTruthy();
+  expect(result.ast).toBeTruthy();
+  expect(result.code).toBeTruthy();
+  expect(result.map).toBeNull();
+});
