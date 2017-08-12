2fb12a3d54e017e27f52868d6b11613ac0b5c126
Jamie Connolly <jamie@jamieconnolly.com>
2017-08-12 21:23:49 +0100

    :sparkles: Look up browserslist from package.json

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 'use strict';
 
+const path = require('path');
+const pkg = require(path.resolve('./package.json'));
+
 const presetEnv = require.resolve('babel-preset-env');
 const presetStage3 = require.resolve('babel-preset-stage-3');
 
 module.exports = function() {
+  const targets = {};
+
+  if (typeof pkg.browserslist === 'object' && pkg.browserslist.length) {
+    targets.browsers = pkg.browserslist;
+  }
+
   return {
     presets: [
-      presetEnv,
+      [presetEnv, {targets}],
       presetStage3,
     ],
   };
