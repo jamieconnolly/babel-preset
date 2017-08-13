0f652f9cd5e2396076bfd6ee6247e72c0a6c75ea
Jamie Connolly <76968+jamieconnolly@users.noreply.github.com>
2017-08-13 11:03:59 +0100

    :sparkles: Look up node version from package.json

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 const path = require('path');
 const pkg = require(path.resolve('./package.json'));
+const semver = require('semver');
+const versions = require('./versions.json');
 
 const presetEnv = require.resolve('babel-preset-env');
 const presetStage3 = require.resolve('babel-preset-stage-3');
@@ -11,6 +13,16 @@ module.exports = function() {
 
   if (typeof pkg.browserslist === 'object' && pkg.browserslist.length) {
     targets.browsers = pkg.browserslist;
+  }
+
+  if (typeof pkg.engines === 'object' && pkg.engines.node) {
+    const version = pkg.engines.node;
+
+    if (semver.valid(version)) {
+      targets.node = version;
+    } else if (semver.validRange(version)) {
+      targets.node = semver.minSatisfying(versions.node, version);
+    }
   }
 
   return {
