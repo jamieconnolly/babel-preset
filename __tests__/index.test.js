9c6ef46cdda947b33c30700c8258e57bfe031cdc
Jamie Connolly <76968+jamieconnolly@users.noreply.github.com>
2017-08-16 11:02:43 +0100

    :hammer: Use arrow functions instead of function expressions

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,14 +3,14 @@
 const babel = require('babel-core');
 const isPlainObj = require('is-plain-obj');
 
-it('should export an object', function() {
+it('should export an object', () => {
   const config = require('../index')();
 
   expect(isPlainObj(config)).toBe(true);
   expect(config.presets).toBeTruthy();
 });
 
-it('should not contain invalid syntax', function() {
+it('should not contain invalid syntax', () => {
   const result = babel.transform('let b = 1; a ** b;', {
     babelrc: false,
     presets: ['./index.js'],
