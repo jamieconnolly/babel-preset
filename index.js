37c3b9ec20d67a855823e4b4024a46433939f767
Jamie Connolly <jamie@jamieconnolly.com>
2017-08-17 10:51:18 +0100

    :sparkles: Transpile modules to CommonJS in the test environment

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 'use strict';
 
 const path = require('path');
-const pkg = require(path.resolve('./package.json'));
 const semver = require('semver');
-const versions = require('./versions.json');
 
 const presetEnv = require.resolve('babel-preset-env');
 const presetStage3 = require.resolve('babel-preset-stage-3');
@@ -11,9 +9,12 @@ const presetStage3 = require.resolve('babel-preset-stage-3');
 module.exports = (context, options) => {
   options = options || {};
 
-  let debug = options.debug || false;
-  let modules = options.modules || false;
-  let targets = {};
+  const env = process.env.BABEL_ENV || process.env.NODE_ENV;
+  const pkg = require(path.resolve('./package.json'));
+
+  const debug = options.debug || false;
+  const modules = options.modules || env === 'test' ? 'commonjs' : false;
+  const targets = {};
 
   if (pkg.browserslist) {
     targets.browsers = pkg.browserslist;
@@ -21,6 +22,7 @@ module.exports = (context, options) => {
 
   if (pkg.engines && pkg.engines.node) {
     const version = pkg.engines.node;
+    const versions = require('./versions.json');
 
     if (semver.valid(version)) {
       targets.node = version;
